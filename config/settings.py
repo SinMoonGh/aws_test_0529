061143674ccaff6c60acf878a2d26fdbe4801f5d
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-05-29 15:04:14 +0900

    done

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4)my*p=_5okhosh)h3al5k1d9)4dlu60nrkr^@^nl9er%)jnzr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
